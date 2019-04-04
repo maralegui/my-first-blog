3bd2609561812ff2562f94360a4e23d441097f90
mara.leguizamon@gmail.com <mara.leguizamon@gmail.com>
2019-04-04 20:55:59 -0300

    Blog working ok.

--- blog/urls.py
+++ blog/urls.py
@@ -1,9 +1,9 @@
 from django.conf.urls import url
 from . import views
-from django.shortcuts import render, get_object_or_404
 
 urlpatterns = [
     url(r'^$', views.post_list, name='post_list'),
     url(r'^post/(?P<pk>\d+)/$', views.post_detail, name='post_detail'),
+    url(r'^post/new/$', views.post_new, name='post_new'),
+    url(r'^post/(?P<pk>\d+)/edit/$', views.post_edit, name='post_edit'),
 ]
-
